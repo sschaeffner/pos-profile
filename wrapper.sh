0f55705bd3fa51e29e0d2e4247a5721477c95a51
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-04-28 11:26:24 +0200

    execute install script as slice owner

--- wrapper.sh
+++ wrapper.sh
@@ -3,4 +3,6 @@
 REPO_DIR=/local/repository
 LOG_DIR=$REPO_DIR
 
-$REPO_DIR/install.sh &> $LOG_DIR/install_log.txt
+USERNAME="$(geni-get user_urn | awk -F+ '{print $4}')"
+
+sudo -u $USERNAME $REPO_DIR/install.sh &> $LOG_DIR/install_log.txt
