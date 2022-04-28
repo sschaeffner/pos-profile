808c8578e0734cff16a586103f0a3e2ba022cdf7
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-04-28 11:40:30 +0200

    install python before using geni-get to get slice owner
    geni-get is a python script but python is not pre-installed on
    Ubuntu 20.04 image.

--- wrapper.sh
+++ wrapper.sh
@@ -3,6 +3,9 @@
 REPO_DIR=/local/repository
 LOG_DIR=$REPO_DIR
 
+sudo apt-get update
+sudo apt-get install -y python3 python-is-python3
+
 USERNAME="$(geni-get user_urn | awk -F+ '{print $4}')"
 
 sudo -u $USERNAME $REPO_DIR/install.sh &> $LOG_DIR/install_log.txt
