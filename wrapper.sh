01a20585d7ab393e39d2386995efe3fd9bd16f96
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-04-28 11:54:34 +0200

    sudo with -H
    such that acting user and $HOME match

--- wrapper.sh
+++ wrapper.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y python3 python-is-python3
 
 USERNAME="$(geni-get user_urn | awk -F+ '{print $4}')"
 
-sudo -u $USERNAME $REPO_DIR/install.sh &> $LOG_DIR/install_log.txt
+sudo -u $USERNAME -H $REPO_DIR/install.sh &> $LOG_DIR/install_log.txt
